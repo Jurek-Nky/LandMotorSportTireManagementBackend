87d291e70109293a8e54c3719bf5dfd6842a43d6
jurek-nky <jurek.nit@gmail.com>
2021-11-14 17:17:00 +0100

    added functions to query the database

--- src/main/java/com/dev/reifen/ReifenRepository.java
+++ src/main/java/com/dev/reifen/ReifenRepository.java
@@ -3,8 +3,13 @@ package com.dev.reifen;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 // ReifenRepository is the lowest layer of the Java backend and is used to interact with the database
 public interface ReifenRepository extends CrudRepository<Reifen, Integer> {
-
+    // Repostitory automatically implements SQL-Querys for the following functions
+    List<Reifen> findReifensByBezeichnung(String bezeichnung);
+    Optional<Reifen> findReifenById(int id);
 }
