5ed79042f65bc51f39d862b6a9e7260d7ac8ff75
jurek-nky <jurek.nit@gmail.com>
2021-11-15 20:32:36 +0100

    changed the config file so that it works with the new Reifen constructor

--- src/main/java/com/dev/reifen/ReifenConfig.java
+++ src/main/java/com/dev/reifen/ReifenConfig.java
@@ -16,16 +16,21 @@ public class ReifenConfig {
     CommandLineRunner commandLineRunner(ReifenRepository repository) {
         return args -> {
             Reifen r1 = new Reifen("298(6ns)", new Date(2021, 6, 2),
-                    new Time(0), "med G/D", "Q2", 20, 90, 90,
+                    new Time(0), "med G/D", "Q2", 1.49, 1.52, 1.33, 1.37, 20, 90, 90,
                     new Time(18, 30, 0), new Time(20, 0, 0));
             Reifen r2 = new Reifen("298(6ns)", new Date(2021, 6, 2),
-                    new Time(0), "med G/D", "Q2", 20, 90, 90,
+                    new Time(0), "med G/D", "Q2", 1.49, 1.52, 1.33, 1.37, 20, 90, 90,
                     new Time(18, 30, 0), new Time(20, 0, 0));
             Reifen r3 = new Reifen("298(6ns)", new Date(2021, 6, 2),
-                    new Time(0), "med G/D", "Q2", 20, 90, 90,
+                    new Time(0), "med G/D", "Q2", 1.49, 1.52, 1.33, 1.37, 20, 90, 90,
                     new Time(18, 30, 0), new Time(20, 0, 0));
+            Reifen r4 = new Reifen("201", new Date(2021, 6, 2),
+                    new Time(17, 50, 0), "med G/D","", 1.49, 1.52, 1.33, 1.37, 20, 90, 90,
+                    new Time(13, 0, 0), new Time(14, 30, 0));
+
             repository.deleteAll();
-            repository.saveAll(List.of(r1, r3, r2));
+
+            repository.saveAll(List.of(r1, r2, r3, r4));
         };
     }
 }
