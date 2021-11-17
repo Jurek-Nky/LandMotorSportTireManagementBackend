c0d045753438241aebbd317fe594266ceac8002b
jurek-nky <jurek.nit@gmail.com>
2021-11-17 18:37:27 +0100

    restructured the code and added more methodes for get requests

--- src/main/java/com/dev/reifen/ReifenService.java
+++ src/main/java/com/dev/reifen/ReifenService.java
@@ -1,12 +1,10 @@
 package com.dev.reifen;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.sql.Date;
-import java.sql.Time;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,24 +19,59 @@ public class ReifenService {
         this.reifenRepository = reifenRepository;
     }
 
-
+    /*##########################################################################################################
+     *  methodes for GET requests
+     */
     public List<Reifen> getReifen() {
-        return (List<Reifen>) reifenRepository.findAll();
+        List<Reifen> reifens = (List<Reifen>) reifenRepository.findAll();
+        if (reifens.isEmpty()) {
+            throw new IllegalStateException("No Tires were found.");
+        }
+        return reifens;
     }
 
+    public List<Reifen> findReifensByBezeichnung(String bezeichnung) {
+        List<Reifen> reifens = reifenRepository.findReifensByBezeichnung(bezeichnung);
+        if (reifens.isEmpty()) {
+            throw new IllegalStateException(String.format("No tires were found with Bezeichnung: %s", bezeichnung));
+        }
+        return reifens;
+    }
+
+    public Optional<Reifen> findReifenById(Long reifenId) {
+        Optional<Reifen> reifen = reifenRepository.findReifenByReifenid(reifenId);
+        if (!reifen.isPresent()) {
+            throw new IllegalStateException(String.format("No tires were found with ReifenID: %s", reifenId));
+        }
+        return reifen;
+    }
+
+    public Optional<Reifen> findReifenBySerialnumber(String serialnumber) {
+        Optional<Reifen> reifen = reifenRepository.findReifenBySerialNumber(serialnumber);
+        if (!reifen.isPresent()) {
+            throw new IllegalStateException(String.format("No tires were found with Serialnumber: %s", serialnumber));
+        }
+        return reifen;
+    }
+
+    public List<Reifen> findReifensByRennId(Long rennid) {
+        List<Reifen> reifens = reifenRepository.findReifenByRennen_Rennid(rennid);
+        if (reifens.isEmpty()) {
+            throw new IllegalStateException(String.format("No tires were found with RennID: %s", rennid));
+        }
+        return reifens;
+    }
+
+    /*##########################################################################################################
+     *  methodes for POST requests
+     */
     public void addNewReifen(Reifen reifen) {
         reifenRepository.save(reifen);
     }
 
-    //@Query("SELECT r from Reifen r WHERE r.bezeichnung = ?1")
-    public List<Reifen> findReifenByBezeichnung(String bezeichnung) {
-        return reifenRepository.findReifensByBezeichnung(bezeichnung);
-    }
-
-    public Optional<Reifen> findReifenById(int id) {
-        return reifenRepository.findReifenByReifenid(id);
-    }
-
+    /*##########################################################################################################
+     *  methodes for DELETE requests
+     */
     public void deleteReifen(int reifenId) {
         if (!reifenRepository.existsById(reifenId)) {
             throw new IllegalStateException("reifen with id " + reifenId + " does not exist.");
@@ -47,6 +80,9 @@ public class ReifenService {
         }
     }
 
+    /*##########################################################################################################
+     *  methodes for PUT requests
+     */
     @Transactional
     public void updateReifen(int reifenId, String bezeichnung, Date datum) {
         Reifen reifen = reifenRepository.findById(reifenId).orElseThrow(() ->
@@ -60,3 +96,4 @@ public class ReifenService {
         }
     }
 }
+
