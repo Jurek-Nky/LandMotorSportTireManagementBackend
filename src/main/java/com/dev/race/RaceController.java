4ce1bae204d973e4f54e8ae44fae586ba09b702a
jurek-nky <jurek.nit@gmail.com>
2021-11-27 13:05:22 +0100

    added descriptions for api documentation

--- src/main/java/com/dev/race/RaceController.java
+++ src/main/java/com/dev/race/RaceController.java
@@ -1,5 +1,6 @@
 package com.dev.race;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +18,19 @@ public class RaceController {
     }
 
     @GetMapping(path = "/all")
+    @ApiOperation(value = "returns all races stored in the system.")
     public List<Race> getAllRaces() {
         return raceService.getRaces();
     }
 
     @GetMapping(path = "/date")
+    @ApiOperation(value = "find races by date.")
     public List<Race> getRacesByDate(@RequestAttribute(name = "date") String date) {
         return raceService.findAllByDate(date);
     }
 
     @PostMapping("/new")
+    @ApiOperation(value = "insert new race.")
     public Race addNewRace(@RequestBody(required = true) Race race) {
         return raceService.addNewRace(race);
     }
