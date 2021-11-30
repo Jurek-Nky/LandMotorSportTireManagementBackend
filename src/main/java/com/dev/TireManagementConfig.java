b56b705f016752a6b8cc20a015558342445011a4
jurek-nky <jurek.nit@gmail.com>
2021-11-30 10:21:36 +0100

    reformatting

--- src/main/java/com/dev/TireManagementConfig.java
+++ src/main/java/com/dev/TireManagementConfig.java
@@ -9,7 +9,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
