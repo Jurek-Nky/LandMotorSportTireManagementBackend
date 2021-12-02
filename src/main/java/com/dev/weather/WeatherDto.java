acc8ac350802e5444855e511afeef8de3d025078
jurek nitschky <jurek.nit@gmail.com>
2021-12-02 19:24:12 +0100

    removed time and date in weatherdto

--- src/main/java/com/dev/weather/WeatherDto.java
+++ src/main/java/com/dev/weather/WeatherDto.java
@@ -1,31 +1,23 @@
 package com.dev.weather;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+@ApiModel
 public class WeatherDto {
 
-    private LocalDate date;
-    private Time time;
     private int airtemp;
     private int tracktemp;
     private String cond;
 
     public WeatherDto(int airTemp, int trackTemp, String condition) {
-        this.date = LocalDate.now();
-        this.time = Time.valueOf(LocalTime.now());
         this.airtemp = airtemp;
         this.tracktemp = tracktemp;
         this.cond = condition;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public Time getTime() {
-        return time;
     }
 
     public int getAirtemp() {
