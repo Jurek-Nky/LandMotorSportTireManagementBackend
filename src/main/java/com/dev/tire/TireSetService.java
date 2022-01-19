9fa0caba41001e95652dc6113dbe6ed202386472
jurek-nky <jurek.nit@gmail.com>
2022-01-19 19:14:31 +0100

    undid changes and instead gave every tire a position attribute

--- src/main/java/com/dev/tire/TireSetService.java
+++ src/main/java/com/dev/tire/TireSetService.java
@@ -85,25 +85,24 @@ public class TireSetService {
         } else {
             nr = lastSet.get().getTireSetNr() + 1;
         }
-        Tire[] tires = new Tire[4];
-        tires[0] = new Tire(tireSet.FL.mischung, tireSet.FL.art);
-        tires[1] = new Tire(tireSet.FR.mischung, tireSet.FR.art);
-        tires[2] = new Tire(tireSet.RL.mischung, tireSet.RL.art);
-        tires[3] = new Tire(tireSet.RR.mischung, tireSet.RR.art);
-        for (Tire tire : tires) {
-            tire.setBestelltUm(Time.valueOf(LocalTime.now()));
-            tire.setTireSet(tireSet);
-            if (tire.getMischung().equals("Heavy_wet") || tire.getMischung().equals("Dry_wet")) {
-                tire.setHeatingTemp(40);
-            } else {
-                tire.setHeatingTemp(90);
+        List<Tire> tires = new ArrayList<>();
+        for (Tire tire : tireSet.tires) {
+            Tire t = new Tire(tire.mischung, tire.art);
+            t.setPosition(tire.position);
+            if (tire.getModification() != null) {
+                t.setModification(tire.modification);
             }
-            tire.setBezeichnung(String.format("%s%02d", race.get().getPrefixes().getprefix(tire.getMischung()), nr));
+            t.setBestelltUm(Time.valueOf(LocalTime.now()));
+            t.setTireSet(tireSet);
+            if (t.getMischung().equals("Heavy_wet") || tire.getMischung().equals("Dry_wet")) {
+                t.setHeatingTemp(40);
+            } else {
+                t.setHeatingTemp(90);
+            }
+            t.setBezeichnung(String.format("%s%02d", race.get().getPrefixes().getprefix(t.getMischung()), nr));
+            tires.add(t);
         }
-        tireSet.setFL(tires[0]);
-        tireSet.setFR(tires[1]);
-        tireSet.setRL(tires[2]);
-        tireSet.setRR(tires[3]);
+        tireSet.setTires(tires);
         tireSet.setTireSetNr(nr);
         tireSet.setRace(race.get());
         tireSet.setStatus("bestellt");
