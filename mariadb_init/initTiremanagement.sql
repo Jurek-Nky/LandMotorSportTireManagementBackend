989773ac0270c16d41cf229051f996783795531f
jurek-nky <jurek.nit@gmail.com>
2022-01-20 16:31:17 +0100

    added dockerfiles for automatic deployment;

---
+++ mariadb_init/initTiremanagement.sql
@@ -0,0 +1,7 @@
+create schema tiremanagement;
+
+create schema tiremanagementTest;
+
+grant all privileges on tiremanagement.* to restapi;
+
+grant all privileges on tiremanagementTest.* to restapi;
