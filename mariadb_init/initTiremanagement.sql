cd9432147843d3ddf28798895c2274ed83f6d8a0
jurek-nky <jurek.nit@gmail.com>
2022-01-26 08:07:40 +0100

    cleaning up

--- mariadb_init/initTiremanagement.sql
+++ mariadb_init/initTiremanagement.sql
@@ -1,7 +1,2 @@
 create schema tiremanagement;
-
-create schema tiremanagementTest;
-
 grant all privileges on tiremanagement.* to restapi;
-
-grant all privileges on tiremanagementTest.* to restapi;
